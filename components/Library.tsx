b69b217b992705151aee44e8b7bd23618f33c5de
Adil C <adilc0070@gmail.com>
2024-08-16 15:23:22 +0530

    side bar basic route

---
+++ components/Library.tsx
@@ -0,0 +1,28 @@
+'use client'
+import React from 'react'
+import { AiOutlinePlus } from 'react-icons/ai'
+import { TbPlaylist } from 'react-icons/tb'
+
+const Library = () => {
+    const onClick = () => {
+        // Handle search
+    }
+    return (
+        <div className="flex flex-col h-full" >
+
+            <div className='flex items-center justify-between px-5 py-4'>
+                <div className='inline-flex items-center gap-x-2'>
+
+                    <TbPlaylist size={26} className='text-neutral-400' />
+                    <p className='text-neutral-400 font-medium text-md'>Your Library</p>
+                </div>
+                <AiOutlinePlus size={20} className='text-neutral-400 cursor-pointer hover:text-white transition' onClick={onClick} />
+            </div>
+            <div className=' flex flex-col gap-y-2 px-3'>
+                list of songs
+            </div>
+        </div>
+    )
+}
+
+export default Library
